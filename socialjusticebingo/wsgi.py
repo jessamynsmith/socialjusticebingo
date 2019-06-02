57cc482c5d42e07b62c1b688eda50a3410d6a0e5
jessamynsmith <jessamyn.smith@gmail.com>
2019-06-01 21:19:50 -0600

    Updated to circleci 2

--- socialjusticebingo/wsgi.py
+++ socialjusticebingo/wsgi.py
@@ -15,12 +15,12 @@ framework.
 """
 import os
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "socialjusticebingo.settings.production")
-
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
 # setting points here.
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling, MediaCling
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "socialjusticebingo.settings.production")
 
 application = Cling(MediaCling(get_wsgi_application()))
